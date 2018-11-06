c3ff1cfb2a7ab5c6ba0b060e10fc79dd2064bf5b
idannaim <idanaim@gmail.com>
2018-11-06 11:28:49 +0200

    split to component and pass add recipe

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React, {Component} from 'react';
 import './App.css';
 
+/**split the code and passing action to components
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
 
 const Recipe = (props) => (
     <li className="recipe">{props.recipe}</li>
-)
+);
 
 const Recipes = (props) => (
     <ul className="recipes">
       {props.recipes.map(recipe => <Recipe key={recipe} recipe={recipe}/>)}
     </ul>
 );
+
+
+class AddRecipe extends Component {
+  constructor() {
+    super();
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.props.addRecipe(this.title);
+    this.title.value = '';
+  }
+
+  render() {
+    return (
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input ref={e => this.title = e} type="text"/>
+          <button>Add</button>
+        </form>
+
+    )
+  }
+
+}
+
 
 class App extends Component {
   constructor() {
@@ -26,27 +57,15 @@ class App extends Component {
     return (
         <div className="App">
           <h3>Recipes:</h3>
-
-          <form onSubmit={this.onSubmit.bind(this)}>
-            <input ref={e => this.title = e} type="text"/>
-            <button>Add</button>
-          </form>
-
+          <AddRecipe addRecipe={this.addRecipe}/>
           <Recipes recipes={this.state.recipes}/>
-
-
         </div>
     );
   }
 
 
-  onSubmit(e) {
-    e.preventDefault();
-
-    const newRecipes = this.state.recipes.concat(this.title.value);
-
-    this.title.value = '';
-
+  addRecipe = (title) => {
+    const newRecipes = this.state.recipes.concat(title.value);
     this.setState({recipes: newRecipes});
   }
 }
