80a262b0dbd4c40af46310eabe874d14cc15b765
idannaim <idanaim@gmail.com>
2018-11-06 10:59:04 +0200

    init basic app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const recipes = ['Waffles', 'Omelets'];
+
+const Recipe = (props) =>(
+    <li>{ props.recipe }</li>
+)
+
+const Recipes = (props) => (
+  <ul>
+    {props.recipes.map(recipe => <Recipe key={recipe} recipe={recipe} />)}
+  </ul>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Recipes:</h1>
+        <Recipes recipes={recipes} />
       </div>
     );
   }
