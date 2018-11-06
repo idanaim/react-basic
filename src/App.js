f166f03de2c80a613050c2fba3923e95149f5b08
idannaim <idanaim@gmail.com>
2018-11-06 11:14:59 +0200

    create state and add form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-const recipes = ['Waffles', 'Omelets'];
 
-const Recipe = (props) =>(
-    <li>{ props.recipe }</li>
+const Recipe = (props) => (
+    <li className="recipe">{props.recipe}</li>
 )
 
 const Recipes = (props) => (
-  <ul>
-    {props.recipes.map(recipe => <Recipe key={recipe} recipe={recipe} />)}
-  </ul>
+    <ul className="recipes">
+      {props.recipes.map(recipe => <Recipe key={recipe} recipe={recipe}/>)}
+    </ul>
 );
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      recipes: ['Waffles', 'Omelets']
+    }
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <h1>Recipes:</h1>
-        <Recipes recipes={recipes} />
-      </div>
+        <div className="App">
+          <h3>Recipes:</h3>
+
+          <form onSubmit={this.onSubmit.bind(this)}>
+            <input ref={e => this.title = e} type="text"/>
+            <button>Add</button>
+          </form>
+
+          <Recipes recipes={this.state.recipes}/>
+
+
+        </div>
     );
+  }
+
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newRecipes = this.state.recipes.concat(this.title.value);
+
+    this.title.value = '';
+
+    this.setState({recipes: newRecipes});
   }
 }
 
