0c3c5fd5aa0155e59ddb5174b1dd6050762c8167
idannaim <idanaim@gmail.com>
2018-11-08 14:21:47 +0200

    propTypes

--- src/App.js
+++ src/App.js
@@ -2,27 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const recipes = ['Waffles', 'Omelette'];
+
+const Recipe = () => (
+    <li>Waffel</li>
+);
+
+const App = () => (
+    <div>
+      <h1>Recipes:</h1>
+      <ul>
+        <Recipe />
+        <Recipe />
+      </ul>
+    </div>
+);
 
 export default App;
